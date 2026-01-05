3517a8734300cfe55a05e1395eac2300595cedeb
Henry Odohi Unah <henry.unah@gmail.com>
2026-01-05 16:41:14 +0100

    update middleware

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,23 +1,22 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 const isPublicRoute = createRouteMatcher([
-    "/signin(.*)",
-    "/signup(.*)",
-    "/forgot-password(.*)",
-    "/verify-email(.*)",
+  "/signin(.*)",
+  "/signup(.*)",
+  "/forgot-password(.*)",
+  "/verify-email(.*)",
+  "/api/stripe/webhook",         // 👈 add this
 ]);
 
 export default clerkMiddleware(async (auth, request) => {
-    if (!isPublicRoute(request)) {
-        await auth.protect();
-    }
+  if (!isPublicRoute(request)) {
+    await auth.protect();
+  }
 });
 
 export const config = {
-    matcher: [
-        // Skip Next.js internals and all static files, unless found in search params
-        '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-        // Always run for API routes
-        '/(api|trpc)(.*)',
-    ],
+  matcher: [
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    '/(api|trpc)(.*)',
+  ],
 };
